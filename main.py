e7c5c98b60e2bb424e322370f16677f23d9dabc4
Serikzhan Kuanyshev <70684876+SekoJuku@users.noreply.github.com>
2023-04-19 19:32:10 +0600

    fix: downloader.py

--- main.py
+++ main.py
@@ -17,10 +17,10 @@ async def docs_redirect():
 @app.get("/download/{url:path}")
 async def download_file(url: str):
     extension = url.split(".")[-1]
-    #file_path = await download_file_to_tempfile(url)
+    file_path = await download_file_to_tempfile(url)
     options = {
         'no-stop-slow-scripts': None,
         "encoding": "UTF-8"
     }
-    pdfkit.from_url(url, "test.pdf", options=options, configuration=pdfkit.configuration(wkhtmltopdf=resp))
+    pdfkit.from_file(file_path, "test.pdf", options=options, configuration=pdfkit.configuration(wkhtmltopdf=resp))
     return StreamingResponse(open('test.pdf', "rb"), media_type='application/pdf')
--- src/downloader.py
+++ src/downloader.py
@@ -11,7 +11,10 @@ async def download_file_to_tempfile(url: str) -> str:
                 raise HTTPException(status_code=404, detail="Failed to download file")
 
             file_content = await response.read()
+            extension = url.split(".")[-1]
 
             with tempfile.NamedTemporaryFile(mode="wb", delete=False) as f:
-                f.write(file_content)
-                return f.name
+                file_name = f.name + "." + extension
+                with open(file_name, 'w') as file:
+                    file.write(file_content)
+                return file_name
